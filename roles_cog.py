6dd03278b6bbe47a2f258105351b767d348a16b2
Laurent Brand <laurentbrand19@gmail.com>
2021-04-14 00:09:32 +0200

    Update roles_cog.py

--- roles_cog.py
+++ roles_cog.py
@@ -4,12 +4,12 @@ from discord.ext import commands, tasks
 class roles_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.loop.start()
+        #self.loop.start()
         
 
     @tasks.loop(seconds=2)
     async def loop(self):
-        pass#role = discord.utils.get(member.server.roles, name="")
+        pass#role = discord.utils.get(guild.roles, name="")
 
 
         
@@ -17,15 +17,3 @@ class roles_cog(commands.Cog):
     async def on_ready(self):
         print(f"Bot started. Latency: {int(self.bot.latency*1000)}ms")
 
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        await self.bot.get_channel(831280507924381697).send(member)
-    
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        await self.bot.get_channel(831280507924381697).send(member)
-
-
-    async def send_to_all(self, msg):
-        for text_channel in self.text_channel_list:
-            await text_channel.send(msg)

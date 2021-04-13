74c1dcef378947f2f54731a7566e3264993cfce4
Laurent Brand <laurentbrand19@gmail.com>
2021-04-13 22:37:07 +0200

    update

---
+++ bot.py
@@ -0,0 +1,22 @@
+import config
+
+import discord
+from discord.ext import commands
+
+import os
+
+#import all of the cogs
+
+from music_cog import music_cog
+
+bot = commands.Bot(command_prefix='.')
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
+
+bot.add_cog(music_cog(bot))
+
+#start the bot with our token
+bot.run(config.token)
